055a37bfc5ea183f3c27ae80f5c166aa51185eb0
kubrakoksal <koksalkubra0@gmail.com>
2024-03-18 22:40:59 +0300

    Fix bugs

--- src/main/java/com/kkoksal/Main.java
+++ src/main/java/com/kkoksal/Main.java
@@ -1,11 +1,31 @@
 package com.kkoksal;
 
+import com.couchbase.client.java.Cluster;
+import com.couchbase.client.java.manager.query.CreatePrimaryQueryIndexOptions;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.couchbase.core.CouchbaseTemplate;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
+
+    @Autowired
+    CouchbaseTemplate couchbaseTemplate;
+
+    @Autowired
+    Cluster cluster;
+
+    @PostConstruct
+    private void postConstruct() {
+
+        cluster.queryIndexes().createPrimaryIndex(couchbaseTemplate.getBucketName(),
+                CreatePrimaryQueryIndexOptions.createPrimaryQueryIndexOptions().ignoreIfExists(true));
+
+    }
+
 }
